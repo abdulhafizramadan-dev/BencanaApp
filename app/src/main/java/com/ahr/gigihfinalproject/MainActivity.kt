d4cc3da9a046ecd87a1cdd6a64863419b8938e46
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2023-07-24 23:24:08 +0700

    refactor: Clearing unused composable function

--- app/src/main/java/com/ahr/gigihfinalproject/MainActivity.kt
+++ app/src/main/java/com/ahr/gigihfinalproject/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.ahr.gigihfinalproject.presentation.main.MainScreen
 import com.ahr.gigihfinalproject.ui.theme.GigihFinalProjectTheme
@@ -31,21 +28,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GigihFinalProjectTheme {
-        Greeting("Android")
     }
 }
